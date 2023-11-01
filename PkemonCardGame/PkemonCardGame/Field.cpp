655fd29696bed55f2db284a281b83268107f60b2
oumadgij <106945464+oumadgij@users.noreply.github.com>
2023-11-01 15:34:26 +0900

    フィールド追加、修正
    バトルフィールドを追加
    線を太くした

--- PkemonCardGame/PkemonCardGame/Field.cpp
+++ PkemonCardGame/PkemonCardGame/Field.cpp
@@ -21,69 +21,82 @@ Field::Field()
 
 	Stadium_X = 200 + STAGE_WIDTH_MIN;
 	Stadium_Y = 400;
+
+	Battle_X = 500 + STAGE_WIDTH_MIN;
+	Battle_Y =	300;
 }
 
 void Field::Draw() const
 {
+	//背景
+	DrawBoxAA((float)STAGE_WIDTH_MIN, 0.0f, (float)STAGE_WIDTH_MAX, (float)SCREEN_HEIGHT, 0x57af72, true);//0x57bf6a
+	/*DrawBoxAA(0.0f, 0.0f, (float)STAGE_WIDTH_MIN, (float)SCREEN_HEIGHT, 0xf5f5f5, true);
+	DrawBoxAA((float)STAGE_WIDTH_MAX, 0.0f, (float)SCREEN_WIDTH, (float)SCREEN_HEIGHT, 0xf5f5f5, true);*/
+
 	for (int i = 0; i < 2; i++)
 	{
 		//トラッシュ
-		int x = Trash_X + (i * (STAGE_WIDTH_MAX * 0.63));
-		int y = Trash_Y + (i * (SCREEN_HEIGHT * 0.8));
-		DrawBox(x, y, x + Size_W, y + Size_H, 0xffffff, false);
-		DrawString(x + 15, y + 20, "トラッシュ", 0xffffff);
+		float x = static_cast<float>(Trash_X + (i * (STAGE_WIDTH_MAX * 0.63)));
+		float y = static_cast<float>(Trash_Y + (i * (SCREEN_HEIGHT * 0.8)));
+		DrawBoxAA(x, y, x + Size_W, y + Size_H, 0xffffff, false, Line_Tchickness);
+		DrawStringF(x + 15, y + 20, "トラッシュ", 0xffffff);
 
 		//デッキ
-		x = Deck_X + (i * (STAGE_WIDTH_MAX * 0.63));
-		y = Deck_Y + (i * (SCREEN_HEIGHT * 0.48));
-		DrawBox(x, y, x + Size_W, y + Size_H, 0xffffff, false);
-		DrawString(x + 15, y + 20, "デッキ", 0xffffff);
+		x = static_cast<float>(Deck_X + (i * (STAGE_WIDTH_MAX * 0.63)));
+		y = static_cast<float>(Deck_Y + (i * (SCREEN_HEIGHT * 0.48)));
+		DrawBoxAA(x, y, x + Size_W, y + Size_H, 0xffffff, false, Line_Tchickness);
+		DrawStringF(x + 15, y + 20, "デッキ", 0xffffff);
 
 		//サイド
 		int correction = 25;  //補正値
-		x = Side_X + STAGE_WIDTH_MAX * 0.63;
+		x = static_cast<float>(Side_X + i*STAGE_WIDTH_MAX * 0.63);
 		for (int h = 0; h < 3; h++)
 		{
 			int interval = 10 * h;
 			if (i != 0) //CPU側
 			{
-				y = Side_Y - SCREEN_HEIGHT * 0.15 - h * Size_H;
+				y = static_cast<float>(Side_Y - SCREEN_HEIGHT * 0.15 - h * Size_H);
 				//後ろ
-				DrawBox(x + correction, y - (correction+interval), x + Size_W + correction, y + Size_H - (correction+interval), 0xffffff, false);
+				DrawBoxAA(x + correction, y - (correction+interval), x + Size_W + correction
+					, y + Size_H - (correction+interval), 0xffffff, false, Line_Tchickness);
 				//線隠す用
-				DrawBox(x + correction, y + 1-interval, x + Size_W, y + Size_H-interval, 0x000000, true);
+				DrawBoxAA(x+1, y + 1-interval, x + Size_W, y + Size_H-interval, 0x57af72, true, Line_Tchickness);
 				//手前
-				DrawBox(x, y-interval, x + Size_W, y + Size_H-interval, 0xffffff, false);
+				DrawBoxAA(x, y-interval, x + Size_W, y + Size_H-interval, 0xffffff, false, Line_Tchickness);
 			}
 			else  //Player側
 			{
 				y = Side_Y + h * Size_H;
 				//後ろ
-				DrawBox(Side_X, y + correction+ interval, Side_X + Size_W, y + Size_H + correction+ interval, 0xffffff, false);
+				DrawBoxAA(Side_X, y + correction+ interval, Side_X + Size_W, y + Size_H + correction+ interval, 0xffffff, false, Line_Tchickness);
 				//線隠す用
-				DrawBox(Side_X + (correction+1), y + 1 + interval, Side_X + Size_W + 2, y + Size_H + interval, 0x000000, true);
+				DrawBoxAA(Side_X + (correction+1), y + 1 + interval, Side_X + Size_W + 2, y + Size_H + interval, 0x57af72, true, Line_Tchickness);
 				//手前
-				DrawBox(Side_X + correction, y + interval, Side_X + Size_W + correction, y + Size_H + interval, 0xffffff, false);
+				DrawBoxAA(Side_X + correction, y + interval, Side_X + Size_W + correction, y + Size_H + interval, 0xffffff, false, Line_Tchickness);
 			}
 		}
-		DrawString(Side_X + (i * (STAGE_WIDTH_MAX * 0.63)) + 15, Side_Y - (i * (SCREEN_HEIGHT * 0.3))+10, "サイド", 0xffffff);
+		DrawStringF(x + 15, static_cast<float>(Side_Y - (i * (SCREEN_HEIGHT * 0.3)) + 10), "サイド", 0xffffff);
 
 		//ベンチ
-		y = Bench_Y + (i * (SCREEN_HEIGHT * 0.8));
+		y = static_cast<float>(Bench_Y + (i * (SCREEN_HEIGHT * 0.8)));
 		for (int w = 0; w < 5; w++)
 		{
 			x = Bench_X + w * (Size_W + 30);
-			DrawBox(x, y, x + Size_W, y + Size_H, 0xffffff, false);
+			DrawBoxAA(x, y, x + Size_W, y + Size_H, 0xffffff, false, Line_Tchickness);
 		}
-		DrawString(Bench_X + 15, Bench_Y + (i * (SCREEN_HEIGHT * 0.8)) + 10, "ベンチ", 0xffffff);
+		DrawStringF(Bench_X + 15, static_cast<float>(Bench_Y + (i * (SCREEN_HEIGHT * 0.8)) + 10), "ベンチ", 0xffffff);
+
+		//バトルフィールド
+		y = static_cast<float>(Battle_Y + (i * (SCREEN_HEIGHT * 0.3)));
+		DrawBoxAA(Battle_X, y, Battle_X + Size_W, y + Size_H, 0xffffff, false, Line_Tchickness);
+		DrawStringF(Battle_X + 15, y + 10, "バトルフィールド", 0xffffff);
 	}
 
 	//スタジアム
-	DrawBox(Stadium_X, Stadium_Y, Stadium_X + Size_W, Stadium_Y + Size_H, 0xffffff, false);
-	DrawString(Stadium_X + 5, Stadium_Y + 10, "スタジアム", 0xffffff);
+	DrawBoxAA(Stadium_X, Stadium_Y, Stadium_X + Size_W, Stadium_Y + Size_H, 0xffffff, false, Line_Tchickness);
+	DrawStringF(Stadium_X + 5, Stadium_Y + 10, "スタジアム", 0xffffff);
 
 	//デッドライン
-	DrawLine(SCREEN_WIDTH * 0.2, 0, SCREEN_WIDTH * 0.2, SCREEN_HEIGHT, 0x0000ff);
-	DrawLine(SCREEN_WIDTH * 0.8, 0, SCREEN_WIDTH * 0.8, SCREEN_HEIGHT, 0x0000ff);
-
+	/*DrawLineAA(SCREEN_WIDTH * 0.2, 0, SCREEN_WIDTH * 0.2, SCREEN_HEIGHT, 0x00ffff);
+	DrawLineAA(SCREEN_WIDTH * 0.8, 0, SCREEN_WIDTH * 0.8, SCREEN_HEIGHT, 0x00ffff);*/
 }
--- PkemonCardGame/PkemonCardGame/Field.h
+++ PkemonCardGame/PkemonCardGame/Field.h
@@ -2,13 +2,15 @@
 class Field
 {
 private:
-	int Side_X, Side_Y;
-	int Deck_X, Deck_Y;
-	int Trash_X, Trash_Y;
-	int Bench_X, Bench_Y;
-	int Stadium_X, Stadium_Y;
+	float Side_X, Side_Y;
+	float Deck_X, Deck_Y;
+	float Trash_X, Trash_Y;
+	float Bench_X, Bench_Y;
+	float Stadium_X, Stadium_Y;
+	float Battle_X, Battle_Y;
 	const int Size_H = 150;
 	const int Size_W = 110;
+	const float Line_Tchickness = 3.0;
 
 public:
 	Field();
