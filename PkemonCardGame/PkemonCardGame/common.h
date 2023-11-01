7088226e349e544965f4f7a446105f9afb4aa7a3
初心者狩りのゾロ <106945336+nakadafumiya@users.noreply.github.com>
2023-11-01 15:37:18 +0900

    一旦

--- PkemonCardGame/PkemonCardGame/common.h
+++ PkemonCardGame/PkemonCardGame/common.h
@@ -3,3 +3,12 @@
 #define SCREEN_HEIGHT 1080
 #define SCREEN_WIDTH 1920
 #define SCREEN_COLOR_BIT_16 16
+// じゃんけん定数
+#define JKN_GOO　　(0)
+#define JKN_CHOKI　(1)
+#define JKN_PA　　(2)
+
+// 勝敗の定数
+#define TYP_KATI　(0)
+#define TYP_MAKE　(1)
+#define TYP_AIKO　(2)
