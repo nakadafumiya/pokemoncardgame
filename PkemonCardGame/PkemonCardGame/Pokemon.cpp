4473463aa959aafac491faac84fe117c500ca4c8
NakayamaKyosuke <107970037+NakayamaKyosuke@users.noreply.github.com>
2023-11-22 09:46:19 +0900

    dwa

--- PkemonCardGame/PkemonCardGame/Pokemon.cpp
+++ PkemonCardGame/PkemonCardGame/Pokemon.cpp
@@ -3,17 +3,39 @@
 
 Pokemon::Pokemon()
 {
-
+	for (int i = 0; i < 20; i++)
+	{
+		fscanf_s(fp, "%s,%d,%s,%s,%d,%d,%d,%s", Poke_id[0].NAME, 20, &Poke_id[0].HP, Poke_id[0].WEEK, 10, Poke_id[0].NOWEEK, 10, &Poke_id[0].RUN, &Poke_id[0].SIDE, &Poke_id[0].EVO, Poke_id[0].SINKAMOTO, 20);
+	}
 	Action = false;
+	if (fp == NULL)
+	{
+		int fclose(FILE * fp);
+		Action = false;
+	}
 }
 
- void Pokemon::Update()
+void Pokemon::Update()
 {
+	{
+		int i = 5;
+		// if (Hand_Card > 19)
+		// {
+			//int i = Hand_Card;
 
-	
+			//Poke_d[i].NAME;
+			//Poke_d[i].HP;
+			//Poke_d[i].WEEK;
+			//Poke_d[i].NOWEEK;
+			//Poke_d[i].RUN;
+			//Poke_d[i].SIDE;
+			//Poke_d[i].EVO;
+			//Poke_d[i].SINKAMOTO;
+		 //}
+
+	}
 }
-
 void Pokemon::Draw() const
 {
-
+	DrawFormatString(0, 0, 0x00FFFF, "%s %d %s %s %d %d %d %s", Poke_id[0].NAME, Poke_id[0].HP, Poke_id[0].WEEK, Poke_id[0].NOWEEK, Poke_id[0].RUN, Poke_id[0].SIDE, Poke_id[0].EVO, Poke_id[0].SINKAMOTO);
 }
